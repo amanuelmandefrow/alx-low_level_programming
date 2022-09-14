77060a278a3e7b47e6ce21f9dcdcc2a5d2f7e16c
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-14 16:45:32 +0300

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonnacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Desc: print first 50 fibonnaci number
+ *
+ * Return: Always 0(Succcess)
+ */
+int main(void)
+{
+	int i, num1, num2, sum;
+
+	num1 = 0;
+	num2 = 1;
+	sum = num1 + num2;
+	printf("%d, %d, ", num1, num2);
+	for (i = 1; i < 49; i++)
+	{
+		num1 = num2;
+		num2 = sum;
+		sum = num1 + num2;
+		if (i == 48)
+			printf("%d\n", sum);
+		else
+			printf("%d, ", sum);
+	}
+	return (0);
+}
