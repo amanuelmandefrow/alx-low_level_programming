a9a0d1a1761793a47dab0953638d6152dcabc52d
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-14 11:59:30 +0300

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "headers.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times with viod return type
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	i = 1;
+
+	while (i <= 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar(10);
+		i++;
+	}
+}
