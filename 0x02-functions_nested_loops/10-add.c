6b20c80e8a176c23b18bac861d0bf390c1c72e7b
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-14 14:51:08 +0300

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "headers.h"
+/**
+ * add - sum up two numbers n and m
+ * @a: int number
+ * @b: int number
+ *
+ * Return: sum of two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
