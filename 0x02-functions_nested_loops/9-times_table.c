c8189cd63cef39a5894c33206cf14e19f617e50d
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-14 14:22:54 +0300

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "headers.h"
+/**
+ * times_table - print the 9 times table
+ *
+ * return: has void retur type
+ */
+void times_table(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			_putchar(i * j);
+			_putchar(' ');
+		}
+		_putchar('\n');
+		i++;
+		j = 0;
+	}
+}
