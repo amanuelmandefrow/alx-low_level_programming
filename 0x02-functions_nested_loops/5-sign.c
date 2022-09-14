bf8142c2752784f36f2159c98700bee39757f12f
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-14 12:40:31 +0300

    task 5 is edited

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,19 +7,22 @@
  */
 int print_sign(int n)
 {
+	int type;
+
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+	type = 1;
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
+		type = 0;
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
+		type = -1;
 	}
+	return (type);
 }
