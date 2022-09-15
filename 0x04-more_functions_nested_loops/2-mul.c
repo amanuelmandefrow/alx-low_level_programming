cd7098585ae5147a531081d2c7f714cc2010dd6b
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-15 21:41:09 +0300

    solution for task 1 - 5 for alx more functions, nested loops project

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiply two integer numbers
+ * @a: first integer number
+ * @b: second integer number
+ * Return: multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (b * a);
+}
