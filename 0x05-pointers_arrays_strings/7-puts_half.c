dbea2238aaa2e5247ff9ca4daf5584638121c0c0
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-20 00:11:35 +0300

    task 7 is edited

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,28 +8,23 @@
  */
 void puts_half(char *str)
 {
-	int c, a;
-	char l;
+	int k, i;
 
-	for (c = 0; str[c] != 0; c++)
+	k = 0;
+
+	while (k >= 0)
 	{
+		if (str[k] == '\0')
+			break;
+		k++;
 	}
-	if (c % 2 == 0)
-	{
-		for ((a = c / 2); str[a] != 0; a++)
-		{
-			l = str[a];
-			_putchar(l);
-		}
-	}
+
+	if (k % 2 == 1)
+		i = k / 2;
 	else
-        {
-		for ((a = (c - 1) / 2); str[a] != 0; a++)
-		{
-			l = str[a];
-			_putchar(l);
-		}
-	}
+		i = (k - 1) / 2;
+
+	for (i++; i < k; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
-
