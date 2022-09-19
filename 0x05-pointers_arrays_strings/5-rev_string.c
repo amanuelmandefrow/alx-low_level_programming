9ffc7e0927c76bd1ec2cd8616dd99ce5fc397b0b
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-19 22:55:22 +0300

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: character pointer
+ *
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i, j, k;
+	char temp;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	for ( j = 0; j < (i - 1); ++j)
+	{
+		for (k = j + 1; k > 0; --k)
+		{
+			temp = s[k];
+			s[k] = s[k - 1];
+			s[k - 1] = temp;
+		}
+	}
+}
