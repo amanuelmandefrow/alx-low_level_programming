3672c2b8869a457909e583b11dc59ced03438518
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-19 23:09:00 +0300

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character of string
+ * starting with first character followed by newline
+ * @str: character pointer
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\0');
+}
