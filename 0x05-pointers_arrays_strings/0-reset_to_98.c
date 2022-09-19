a7e783a08a090267697e47f0112323d77da4721c
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-19 21:13:53 +0300

    task 0 again

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,7 @@
 
 /**
  * reset_to_98 - takes pointer to an int as parameter and updates the value
- * @n : integer pointer
- * 
+ * @n : integer pointer 
  * Return: nothing
  */
 void reset_to_98(int *n)
