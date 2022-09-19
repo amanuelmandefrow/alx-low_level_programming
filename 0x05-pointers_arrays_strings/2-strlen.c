cc2c79ed0d6d9cf3d92c970c7c2e1a6fb2ab38e7
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-19 21:34:59 +0300

    task 0

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _strlen - return the length of string
- * @s : character pointer
+ *_strlen - return the length of string
+ *@s : character pointer
  *
  * Return : integer number
  */
@@ -11,7 +11,7 @@ int _strlen(char *s)
 	int k;
 
 	k = 0;
-	while ( *(s + k) != '\0')
+	while (*(s + k) != '\0')
 	{
 		k++;
 	}
