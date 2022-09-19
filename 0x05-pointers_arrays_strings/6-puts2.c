b32584ec58d92e3b1f380ea36b3901105c6670e2
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-20 00:07:31 +0300

    task 6 is edited

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,18 +9,17 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (*(str + i) != '\0')
+	while (i >= 0)
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		else if (str[i] == '\0')
+		if (str[i] == '\0')
 		{
 			_putchar('\n');
 			break;
 		}
+		if (i % 2 == 0)
+			_putchar(str[i]);
 		i++;
 	}
 }
