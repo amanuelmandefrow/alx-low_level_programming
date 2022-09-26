87c82c2b2263000545504fd3601dc3b57b68879f
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-27 01:46:51 +0300

    task 2 solution is edited

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int k;
-
-	for (k = 0; *(s + i) != '\0', ++i)
-		if (s[i] == c)
-			return (s + i);
-	if (s[i] == c)
-		return (s + i);
-	return ('\0');
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	else
+		return ('\0');
 }
