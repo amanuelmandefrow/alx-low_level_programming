6a45bfe598b0ed088a36982b2258f4572c2af495
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-29 00:31:42 +0300

    advanced tasks solutions are added

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,32 +1,44 @@
 #include "main.h"
 
 /**
- * reverse - find reverse of the string
- * @s: char type string
- *
- * Return: nothing
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Return: the length of a string.
  */
-void reverse(char *s)
+int _strlen_recursion(char *s)
 {
-	if (*s)
-	{
-		reverse(s+1);
-		_putchar(*s);
-	}
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * is_palindrome - returns 1 if string is palindrome otherwise 0
- * @s: char type pointer string
- *
- * Return: 1 if string palindrome 0 if not
+ * comparator - compares each character of the string.
+ * @s: string
+ * @n1: smallest iterator.
+ * @n2: biggest iterator.
+ * Return: .
+ */
+int comparator(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - detects if a string is a palindrome.
+ * @s: string.
+ * Return: 1 if s is a palindrome, 0 if not.
  */
 int is_palindrome(char *s)
 {
- char str[] = void reverse(*s);
-
-	if (*str == *s)
+	if (*s == '\0')
 		return (1);
-	else
-		return (0);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
