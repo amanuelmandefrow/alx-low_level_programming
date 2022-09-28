16251be2094aa1602fab0a1c1a98c0eb10233482
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-28 22:33:23 +0300

    again task 4 solution is edited

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns the value of  x raised to 
- * the power of y
+ * _pow_recursion - returns the value of  x raised to the power of y
  * @x: integer number
  * @y: integer number
  *
@@ -14,5 +13,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	else if (y < 0)
 		return (-1);
-	return x * _pow_recursion(x, y - 1);
+	return (x * _pow_recursion(x, y - 1));
 }
