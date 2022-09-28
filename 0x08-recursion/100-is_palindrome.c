d6ec9aacff1f34c9c73bd73e0ec85f4809ee6580
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-09-29 00:15:47 +0300

    solution for task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * reverse - find reverse of the string
+ * @s: char type string
+ *
+ * Return: nothing
+ */
+void reverse(char *s)
+{
+	if (*s)
+	{
+		reverse(s+1);
+		_putchar(*s);
+	}
+}
+
+/**
+ * is_palindrome - returns 1 if string is palindrome otherwise 0
+ * @s: char type pointer string
+ *
+ * Return: 1 if string palindrome 0 if not
+ */
+int is_palindrome(char *s)
+{
+	char *str = reverse(char *s);
+
+	if (*str = *s)
+		return (1);
+	else
+		return (0);
+}
