943bd6c0387957818e0a2ed32ea20c385c913321
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-10-06 13:56:56 +0300

    task 2 is edited

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,14 +11,14 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr ;
+	int *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(sizeof(int) * size);
 	if (ptr == NULL)
 		retrun (NULL);
-	for (i = 0; i <size ; i++)
+	for (i = 0; i < nmemb * size ; i++)
 	{
 		ptr[i] = nmemb[i];
 	}
