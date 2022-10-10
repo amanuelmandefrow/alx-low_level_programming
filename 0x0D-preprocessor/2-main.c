19c51a031a9626b34673f268e84abf901d1ed298
amanuelmadefrow <amanuelmandefrow@gmail.com>
2022-10-10 15:09:54 +0300

    solution of task 2 is edited

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/** 
- * main - Entry point 
+
+/**
+ * main - prints the name of the file.
  *
- * Return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
